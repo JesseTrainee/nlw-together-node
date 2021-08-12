d48e7e4b3e7f38aba06b906a34987a82b817bf4b
JesseTrainee <a.l.i.t.t.l.e@hotmail.com>
2021-08-12 08:44:38 -0300

    aula 03

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,31 @@
+import { UserRepositories } from "../repositories/UsersRepositories";
+import { getCustomRepository } from 'typeorm';
+interface IUserRequest {
+    name: string,
+    email:string,
+    admin?: boolean 
+}
+class CreateUserService {
+    async execute({ name,email,admin } : IUserRequest) {
+        const userRepositories = getCustomRepository(UserRepositories);
+
+        if(!email) {
+            throw new Error("Email incorrect");
+        }
+        const userAlreadyExists = await userRepositories.findOne({
+            email,
+        });
+
+        if(userAlreadyExists){
+            throw new Error("User already exists");
+        }
+
+        const user = userRepositories.create({
+            name,email,admin
+        });
+
+        await userRepositories.save(user);
+        return user;
+    }
+}
+export { CreateUserService };
